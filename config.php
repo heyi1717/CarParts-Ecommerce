c0144720aa55040a57a8d16d85d55e0b22082783
Heyi(Kris) Liu <heyi1717@gmail.com>
2019-07-16 00:02:58 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
    //Specify the Database server host
-   define('DB_SERVER', 'earth.cs.utep.edu');
+   define('DB_SERVER', 'server');
    //Specify the Database username
-   define('DB_USERNAME', 'hliu2');
+   define('DB_USERNAME', 'username');
    //Specify the Database password
-   define('DB_PASSWORD', 'cs5339!hliu2');
+   define('DB_PASSWORD', 'pw');
    //Choose the Database(name)
-   define('DB_DATABASE', 'f17cs4339team11');
+   define('DB_DATABASE', 'db');
    //We make the connection.
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
